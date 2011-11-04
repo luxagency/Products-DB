ba3c2161141f21c783a3751f3b623334da8465f8
Anton <antonzaycev55@gmail.com>
2011-11-04 16:00:10 +0700

    small changes

--- app.rb
+++ app.rb
@@ -7,13 +7,12 @@ require "uri"
 require "haml"
 require "bundler/setup"
 
-
 # App
 require "./helpers.rb"
 require "./models.rb"
 
 dbconfig = YAML.load(File.read("config/database.yml"))
-RACK_ENV ||= ENV["RACK_ENV"] || "development"
+RACK_ENV = ENV["RACK_ENV"] || "development"
 SITE_ID = 1
 ActiveRecord::Base.establish_connection dbconfig[RACK_ENV]
 ActiveRecord::Base.logger = Logger.new(File.open("log/#{RACK_ENV}.log", "a"))
